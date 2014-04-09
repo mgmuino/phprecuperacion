a66d905a5d03d5c87733097e95aecd48e330117a
Gabino (Profesor) <gabino.profesor@gmail.com>
2014-04-09 10:54:11 +0200

    Update recoge.php

--- formulario2/recoge.php
+++ formulario2/recoge.php
@@ -1,6 +1,8 @@
 <?php 
 
-print "<p>Su nombre es                  $_REQUEST[nombre]</p>"; 
+$nombre = $_REQUEST[nombre];
+
+print "<p>Su nombre es                  $nombre</p>"; 
 print "<p>Su fecha de nacimiento es     $_REQUEST[fechanacimiento]</p>"; 
 print "<p>Su email es                   $_REQUEST[email]</p>"; 
 ?>
